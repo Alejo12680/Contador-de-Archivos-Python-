92023581ba153a74d560fc6fab30bdc5805810bd
Alejo12680 <alejo152725@gmail.com>
2024-03-05 09:24:35 -0500

    primer comentario

---
+++ main.py
@@ -0,0 +1,25 @@
+# Importaciones
+
+import os
+
+rutaCarpeta = 'C:/Users/ALEJANDRO/Desktop/AppContadorCarpeta/CarpetaArchivos'
+
+
+def contar_archivos_en_carpeta(carpeta):
+    total_archivos = 0
+    for ruta, carpetas, archivos in os.walk(carpeta):
+        total_archivos += len(archivos)
+    return total_archivos
+
+
+def main():
+    carpeta = rutaCarpeta
+    total_archivos = contar_archivos_en_carpeta(carpeta)
+    ruta_txt = os.path.abspath(os.path.join(os.getcwd(), "../", "../", "total_archivos.txt"))
+    with open(ruta_txt, "w") as archivo_txt:
+        archivo_txt.write(f"El total de archivos {total_archivos}")
+
+
+if __name__ == "__main__":
+    main()
+    print('se logro')
